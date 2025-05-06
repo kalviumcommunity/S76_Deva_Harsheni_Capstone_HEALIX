34ad5854cb662477b0f03a9184dc597004b0b725
harsheni10160 <devaharsheni2006@gmail.com>
2025-05-06 14:33:01 +0530

    Completed testing with bruno

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -11,11 +11,8 @@ exports.protect = async (req, res, next) => {
   if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
     token = req.headers.authorization.split(' ')[1];
   } 
-  // Also check for token in cookies if you want to support that
-  // else if (req.cookies.token) {
-  //   token = req.cookies.token;
-  // }
-
+ 
+  
   // Check if token exists
   if (!token) {
     return res.status(401).json({ 
